30649b995d01a53265e9307a9379afbe2cb78592
DevandScorp <artemda459@gmail.com>
2018-09-11 23:44:10 +0300

    #0

--- src/index.js
+++ src/index.js
@@ -4,4 +4,42 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+    // your implementation
+    // var arr = [-1,...preferences];
+    // var count = 0;
+    // for(let i = 1;i<arr.length;++i) {
+    //     if (arr[arr[arr[i]]] === i) {
+    //         ++count;
+    //         i += 3;
+    //     }
+    // }
+    // return count;
+    // var arr = [-1,...preferences];
+    // var count = 0;
+    // for(let i = 1;i<arr.length;++i) {
+    //     if (arr[arr[arr[i]]] === i && arr[i]!==i) {
+    //         ++count;
+    //         i += 3;
+    //     }
+    // }
+    // var arr = [-1,...preferences];
+    // var count = 0;
+    // for(let i = 1;i<arr.length;) {
+    //     if (arr[arr[arr[i]]] === i && arr[i]!==i) {
+    //         ++count;
+    //
+    //         // console.log('i = ' + i);
+    //     }
+    //     i += 3;
+    // }
+    // return count;
+    var arr = [-1,...preferences];
+    var count = 0;
+    for(let i = 1;i<arr.length;) {
+        if (arr[arr[arr[i]]] === i){// && (arr[arr[i]]!=i || arr[i]!=i)
+            ++count;
+        }
+        i += 3;
+    }
+    return count;
 };

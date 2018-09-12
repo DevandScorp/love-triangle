070714c2620bab1bc66f2af0735a8d446729d81a
DevandScorp <artemda459@gmail.com>
2018-09-12 22:21:41 +0300

    #0

--- src/index.js
+++ src/index.js
@@ -4,42 +4,44 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
-    // your implementation
-    // var arr = [-1,...preferences];
-    // var count = 0;
-    // for(let i = 1;i<arr.length;++i) {
-    //     if (arr[arr[arr[i]]] === i) {
-    //         ++count;
-    //         i += 3;
-    //     }
-    // }
-    // return count;
-    // var arr = [-1,...preferences];
-    // var count = 0;
-    // for(let i = 1;i<arr.length;++i) {
-    //     if (arr[arr[arr[i]]] === i && arr[i]!==i) {
-    //         ++count;
-    //         i += 3;
-    //     }
-    // }
     // var arr = [-1,...preferences];
     // var count = 0;
     // for(let i = 1;i<arr.length;) {
-    //     if (arr[arr[arr[i]]] === i && arr[i]!==i) {
+    //     if (arr[arr[arr[i]]] === i){// && (arr[arr[i]]!=i || arr[i]!=i)
     //         ++count;
-    //
-    //         // console.log('i = ' + i);
     //     }
-    //     i += 3;
+    //     i +=3;
     // }
     // return count;
+    function equals(a,b) {
+        for(let i = 0;i<a.length;++i){
+            if(a[i]!==b[i])return false;
+        }
+        return true;
+    }
+    function contains(arr,elem){
+        for(var a of arr){
+            if(equals(elem,a))return true;
+        }
+        return false;
+    }
+    function compareNumeric(a, b) {
+        if (a > b) return 1;
+        if (a < b) return -1;
+    }
+    var res = [];
     var arr = [-1,...preferences];
     var count = 0;
-    for(let i = 1;i<arr.length;) {
-        if (arr[arr[arr[i]]] === i){// && (arr[arr[i]]!=i || arr[i]!=i)
-            ++count;
+    for(let i = 1;i<arr.length;++i) {
+        if (arr[arr[arr[i]]] === i && (arr[arr[i]]!=i || arr[i]!=i)) {
+            let elem = [arr[i],arr[arr[i]],arr[arr[arr[i]]]].sort(compareNumeric);
+            if(!contains(res,elem)){
+                ++count;
+                res.push(elem);
+            }
+
+            // console.log('i = ' + i);
         }
-        i += 3;
     }
     return count;
 };
